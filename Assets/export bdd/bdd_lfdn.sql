f55e8a2e6b66e07e95f7ef8a160f017928ecae27
lucaslablache <49373835+lucaslablache@users.noreply.github.com>
2021-03-22 16:37:52 +0100

    maj de l'export BDD

--- Assets/export bdd/bdd_lfdn.sql
+++ Assets/export bdd/bdd_lfdn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 30 oct. 2020 à 16:44
+-- Généré le :  lun. 22 mars 2021 à 15:37
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -25,34 +25,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `legumes`
+-- Structure de la table `market`
 --
 
-DROP TABLE IF EXISTS `legumes`;
-CREATE TABLE IF NOT EXISTS `legumes` (
+DROP TABLE IF EXISTS `market`;
+CREATE TABLE IF NOT EXISTS `market` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(20) NOT NULL,
-  `variete` varchar(20) NOT NULL,
-  `photo` varchar(255) NOT NULL,
-  `prix` varchar(20) NOT NULL,
-  `disponibilite` varchar(20) NOT NULL,
+  `date` date NOT NULL,
+  `heure_debut` varchar(255) NOT NULL,
+  `heure_fin` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `information` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
--- Structure de la table `login`
+-- Déchargement des données de la table `market`
 --
 
-DROP TABLE IF EXISTS `login`;
-CREATE TABLE IF NOT EXISTS `login` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `nom_utilisateur` varchar(25) NOT NULL,
-  `mot_de_passe` varchar(128) NOT NULL,
-  `role` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `market` (`ID`, `date`, `heure_debut`, `heure_fin`, `adresse`, `information`) VALUES
+(1, '2020-11-10', '17:05', '17:05', 'ici', ''),
+(2, '2020-11-27', '20:42', '22:44', 'la-bas', ''),
+(4, '2020-12-17', '04:56', '19:56', 'par la', ''),
+(5, '2020-12-25', '20:18', '22:20', 'place de noel', ''),
+(6, '2020-12-22', '06:49', '09:52', 'lavernose', 'producteur 4'),
+(7, '2021-01-12', '06:00', '08:03', 'dtc', 'salut il y auras michel et toto qui vendent des navets'),
+(8, '2021-01-27', '08:36', '22:40', 'ici', 'producteur 4'),
+(9, '2021-02-01', '02:14', '06:18', 'chez pierre', 'ludo'),
+(10, '2021-02-10', '13:00', '14:00', 'chez moi', 'il y auras mon pote qui vend des navets et des patates'),
+(11, '2021-02-07', '15:30', '18:33', 'chez romain', 'salut il y auras michel et toto qui vendent des navets'),
+(12, '2021-02-13', '07:05', '12:38', 'lavernose', 'il y auras mon pote qui vend des navets et des patates'),
+(13, '2021-02-23', '16:00', '17:00', 'Ici', 'La bas'),
+(14, '2021-03-20', '20:02', '23:33', 'ici', 'salut il y auras michel et toto qui vendent des navets'),
+(15, '2021-03-24', '06:46', '09:46', 'chez moi', 'il y auras mon pote qui vend des navets');
 
 -- --------------------------------------------------------
 
@@ -68,9 +73,52 @@ CREATE TABLE IF NOT EXISTS `post` (
   `POST_TYPE` int(11) NOT NULL,
   `POST_AUTEUR` int(11) NOT NULL,
   `POST_DATE` varchar(255) NOT NULL,
-  PRIMARY KEY (`POST_ID`),
-  KEY `auteur` (`POST_AUTEUR`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `POST_PHOTO` varchar(255) NOT NULL,
+  PRIMARY KEY (`POST_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `post`
+--
+
+INSERT INTO `post` (`POST_ID`, `POST_TITRE`, `POST_TEXTE`, `POST_TYPE`, `POST_AUTEUR`, `POST_DATE`, `POST_PHOTO`) VALUES
+(2, 'j\'ai bu mon coca', 'c\'est délicieux j\'ai adoré ca le coca c\'est excellent', 0, 0, '2020-12-07 03:26:28', 'un jour p-e'),
+(6, 'coucou', '&lt;p&gt;salut mon pote&lt;/p&gt;', 1, 0, '2020-12-13 23:19:50', 'un jour p-e'),
+(7, 'coucou', '&lt;p&gt;salut mon pote&lt;/p&gt;', 1, 0, '2020-12-13 23:30:15', 'un jour p-e'),
+(8, 'qsdfqs', '&lt;p&gt;sqdfqs&lt;/p&gt;', 0, 0, '2020-12-14 15:45:32', 'sdqf');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `variete` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `prix` float NOT NULL,
+  `mod_prix` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `saison` int(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`ID`, `nom`, `variete`, `photo`, `prix`, `mod_prix`, `quantite`, `saison`) VALUES
+(1, 'patate', 'jaune', 'lien vers photo', 3, 0, 1, 0),
+(2, 'patate', 'rose', 'lien vers photo', 2, 0, 126, 1),
+(3, 'oeuf', 'gros', 'lien vers photo', 4.89, 2, 622, 1),
+(4, 'courgette', '', 'lien vers photo', 1.65, 1, 21, 1),
+(5, 'haricot', 'vert', 'lien random', 4.51, 0, 542, 1),
+(6, 'poivron', 'rouge', 'photo de poivron', 6.54, 1, 10, 1),
+(7, 'porcelet', 'de lait', 'porc', 15.21, 1, 5, 1),
+(8, 'tomates', 'ronde', 'sfqkdfgqs', 5.21, 0, 40, 2);
 
 -- --------------------------------------------------------
 
@@ -81,22 +129,51 @@ CREATE TABLE IF NOT EXISTS `post` (
 DROP TABLE IF EXISTS `recettes`;
 CREATE TABLE IF NOT EXISTS `recettes` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(20) NOT NULL,
+  `titre` varchar(50) NOT NULL,
+  `description` text NOT NULL,
   `photo` varchar(255) NOT NULL,
-  `ingredients` text NOT NULL,
-  `consignes` text NOT NULL,
+  `saison` int(255) NOT NULL,
+  `ingredient` text NOT NULL,
+  `instruction` text NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
--- Contraintes pour les tables déchargées
+-- Déchargement des données de la table `recettes`
 --
 
+INSERT INTO `recettes` (`ID`, `titre`, `description`, `photo`, `saison`, `ingredient`, `instruction`) VALUES
+(1, 'titre', 'desc', 'photo', 0, 'ingr1§ingr2§ingr2', 'instru1§instru2'),
+(2, 'tartifloute', 'c\'est délicieux', 'gratinée', 0, 'fromage§patates§fromage', 'couper§dans le plat§au four'),
+(3, 'BBQ', 'bonne saucisse', 'ca fume', 2, 'saucisses§cotes de PORC', 'allumer§poser§manger'),
+(4, 'pate au beurre', 'des pates', 'un plat', 1, 'pates§beurre', 'bouillir§plonger§egoutter'),
+(5, 'moules frites', 'les moules en été c\'est génial', 'sdgfsdfgs', 2, 'moules§frites', 'cuire les moules §cuire les frites §déguster§se régaler');
+
+-- --------------------------------------------------------
+
 --
--- Contraintes pour la table `post`
+-- Structure de la table `user`
 --
-ALTER TABLE `post`
-  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`POST_AUTEUR`) REFERENCES `login` (`ID`);
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`ID`, `email`, `nom`, `prenom`, `mot_de_passe`, `role`) VALUES
+(2, 'lucas.lablache@gmail.com', 'Lablache', 'Lucas', '$2y$10$2IQzXsu4KvZvkZ.CMr3Gg.GhPOFl1LBtNbm37oY40x9I4wdNyg.J2', 0),
+(3, 'admin@ferme.com', 'romain', 'K', '$2y$10$1lhHaYJMDxrjw5ngao.LrunDmxJpcPVePXosCt7.OSQewi9SYh1iy', 1),
+(4, 'jacky@test.com', 'jacky', 'test', '$2y$10$r1p4qDOZbtQuPa1aoQ8lyuP0jTa1XAuwrWsibrOBbxv7lQpb4nyAW', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
